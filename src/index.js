ba1eb010ce0c69c1e29b78ca0f4adae3ddfa98b7
clediano <cledianoestefenon@gmail.com>
2019-07-01 20:21:13 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const path = require('path');
+const ForestAdmin = require('forest-express-sequelize');
+const { sequelize } = require('./models');
+const secret = require('../config/secret');
 const express = require('express');
 
 const app = express();
+
+app.use(require('forest-express-sequelize').init({
+    modelsDir: __dirname + '/models',
+    envSecret: secret.FOREST_ENV_SECRET,
+    authSecret: secret.FOREST_AUTH_SECRET,
+    sequelize: sequelize,
+}));
 
 //arquivos estáticos
 app.use(express.static('public'));
